cc8bf4b258edd787f6d593ea453510fb80c4d9ab
Quentin Petel <contact@quentinpetel.fr>
2024-02-22 11:15:11 +0100

    feat: Modification des colonnes de l'export CSV

--- server/src/logic/finder/mefsFinder.js
+++ server/src/logic/finder/mefsFinder.js
@@ -1,3 +1,27 @@
+const { getModels } = require("@mission-apprentissage/tco-service-node");
+const logger = require("../../common/logger");
+
+const completeDateFermetureMefs = async (mefs) => {
+  const Models = await getModels();
+  return await Promise.all(
+    mefs.map(async (mef) => {
+      try {
+        const { DATE_FERMETURE } = await Models.BcnNMef.findOne({ MEF: mef.mef10 });
+
+        const dateParts = DATE_FERMETURE?.split("/");
+
+        return {
+          ...mef,
+          date_fermeture: DATE_FERMETURE ? new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]) : null,
+        };
+      } catch (error) {
+        logger.error({ type: "logic" }, `${mef.mef10} not found in BcnNMef`);
+        return mef;
+      }
+    })
+  );
+};
+
 const computeMefs = async (fields) => {
   let bcn_mefs_10 = fields.bcn_mefs_10;
   let duree_incoherente = false;
@@ -29,6 +53,8 @@ const computeMefs = async (fields) => {
         return modalite.annee !== annee;
       });
   }
+
+  bcn_mefs_10 = await completeDateFermetureMefs(bcn_mefs_10);
 
   return {
     bcn_mefs_10,
