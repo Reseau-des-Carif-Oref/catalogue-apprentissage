935e766078b964ec42a4db62e4ffa5622380a776
ljaymot <luc.jaymot@intercariforef.org>
2025-04-17 09:43:06 +0200

    eslint

--- ui/src/common/components/Search/constantsFormations.js
+++ ui/src/common/components/Search/constantsFormations.js
@@ -609,9 +609,7 @@ const facetDefinition = () => [
       // Calcul simple sans indentation problématique
       const total = data.reduce((acc, curr) => acc + curr.doc_count, 0);
       const oui = data
-        .filter(
-          (d) => d.key && d.key.endsWith && d.key.endsWith("99999#LAD")
-        )
+        .filter((d) => d.key && d.key.endsWith && d.key.endsWith("99999#LAD"))
         .reduce((acc, curr) => acc + curr.doc_count, 0);
       const non = total - oui;
       return [
