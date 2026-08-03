aa9e1aa2e6cb294b3934796dd18f4e9b8594ee0b
ljaymot <luc.jaymot@intercariforef.org>
2026-08-03 11:41:04 +0200

    Pas d'icon warning

--- ui/src/common/components/LiquidationBadge.jsx
+++ ui/src/common/components/LiquidationBadge.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Badge, Flex, Text } from "@chakra-ui/react";
-import { WarningLine } from "../../theme/components/icons";
 
 export const isLiquidation = (value) => value === true || value === "true" || value === 1 || value === "1";
 
@@ -15,7 +14,6 @@ export const LiquidationBadge = ({ value, ...props }) => {
         <Text mx={1} as={"span"}>
           Liquidation judiciaire
         </Text>
-        <WarningLine ml={1} />
       </Flex>
     </Badge>
   );
